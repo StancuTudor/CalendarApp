bd2a93144de5615c0ad336ce4c702d159f4a8dd6
TUDORPC\User <stancutudor17@gmail.com>
2024-06-21 16:30:33 +0300

    Added scrolling.

--- CalendarApp/CalendarApp/FormCalendar.cs
+++ CalendarApp/CalendarApp/FormCalendar.cs
@@ -5,6 +5,9 @@ namespace CalendarApp
         private List<Event> events;
         private List<ListView> listViewDays = [];
         private DateTime currentMonth { get => dtpMonth.Value; set => dtpMonth.Value = value; }
+
+        private DateTime firstOfMonth { get => currentMonth; }
+        private DateTime lastOfMonth { get => currentMonth.AddMonths(1).AddDays(-1); }
         private int initialPanelHeight;
         private int initialFormHeight;
         private int rowHeight;
@@ -43,9 +46,6 @@ namespace CalendarApp
         {
             EmptyCalendar();
 
-            DateTime firstOfMonth = currentMonth;
-            DateTime lastOfMonth = currentMonth.AddMonths(1).AddDays(-1);
-
             int daysInMonth = lastOfMonth.Day;
             int firstDayToFill = (int)(firstOfMonth.DayOfWeek + 6) % 7;
             int lastDayToFill = firstDayToFill + daysInMonth;
@@ -53,14 +53,24 @@ namespace CalendarApp
             // Resize the calendar if too small
             ResizeCalendar(lastDayToFill);
 
+            // Get events for this month
+            var eventsForThisMonth = events.Where(IsEventInMonth).ToList();
+
             // Fill days
             for (int i = firstDayToFill; i < lastDayToFill; i++)
             {
                 int day = i - firstDayToFill + 1;
-                listViewDays[i].Columns.Add(day.ToString(), listViewDays[i].Width - 4);
-                InsertEvents(listViewDays[i], day);
+                listViewDays[i].Columns.Add(day.ToString(), listViewDays[i].Width - 24);
+                InsertEvents(listViewDays[i], day, eventsForThisMonth);
                 listViewDays[i].Show();
             }
+        }
+
+        private bool IsEventInMonth(Event ev)
+        {
+            DateTime startDate = ev.StartDate == null ? DateTime.MinValue : ev.StartDate.Value;
+            DateTime endDate = ev.EndDate == null ? DateTime.MinValue : ev.EndDate.Value;
+            return startDate <= lastOfMonth && endDate >= firstOfMonth;
         }
 
         private void ResizeCalendar(int lastDayToFill)
@@ -82,10 +92,10 @@ namespace CalendarApp
             pnlDays.Height = panelHeight;
         }
 
-        private void InsertEvents(ListView listView, int day)
+        private void InsertEvents(ListView listView, int day, List<Event> eventsList)
         {
             var thisDate = new DateTime(currentMonth.Year, currentMonth.Month, day);
-            foreach (var ev in events)
+            foreach (var ev in eventsList)
             {
                 if (IsDateInEvent(thisDate, ev))
                 {
@@ -129,6 +139,25 @@ namespace CalendarApp
         private void dtpMonth_ValueChanged(object sender, EventArgs e)
         {
             FillCalendarForMonth();
+        }
+
+        private void listView_Scrolled(object sender, ScrollEventArgs e)
+        {
+            var senderTopItemIndex = ((ScrollingListView)sender).TopItem?.Index;
+            if (senderTopItemIndex == null)
+                return;
+
+            foreach (var control in pnlDays.Controls)
+            {
+                if (control is ScrollingListView)
+                {
+                    var currentControl = (ScrollingListView)control;
+                    if (currentControl.Items.Count - 1 < senderTopItemIndex)
+                        continue;
+                    var topItem = currentControl.Items[senderTopItemIndex.Value];
+                    ((ScrollingListView)control).TopItem = topItem;
+                }
+            }
         }
     }
 }
